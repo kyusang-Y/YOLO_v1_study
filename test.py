b4d72fad240afc223a48fecf61483a2dc4583207
kyusang-Y <53461078+kyusang-Y@users.noreply.github.com>
2021-08-10 16:36:39 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,10 @@
 import torch
-from torch._C import dtype
-from torch.nn.modules.activation import Threshold
-from torch.utils.data import Dataset, DataLoader, ConcatDataset
-from torchvision import transforms, models
+from torch.utils.data import DataLoader
 import os
 import cv2
 import numpy as np
-from PIL import Image
-from torch.optim import lr_scheduler
 import torch.nn as nn
-import torch.optim as optim
-from train_1 import FinetuneResnet
-
+from train import NUM_WORKERS, BATCH_SIZE
 class_dict = {"aeroplane":1, "bicycle":2, "bird":3, "boat":4, "bottle":5, 
 "bus":6, "car":7, "cat":8, "chair":9, "cow":10, 
 "diningtable":11, "dog":12, "horse":13, "motorbike":14, "person":15,
@@ -20,7 +13,10 @@ class_dict = {"aeroplane":1, "bicycle":2, "bird":3, "boat":4, "bottle":5,
 list_of_key = list(class_dict.keys())
 list_of_value = list(class_dict.values())
 
-test_dir = './data/temp/test'
+# test_dir = './data/temp/train/JPEGImages'
+# test_dir = './data/temp/train_sample/JPEGImages'   # 1장
+test_dir = './data/temp/train/JPEGImages'    # 14장
+# test_dir = './data/VOCdevkit/test/VOCdevkit/VOC2007/JPEGImages'
 test_files = os.listdir(test_dir)
 pixel_size = 448
 class VOCDataset_test(torch.utils.data.Dataset):
@@ -61,71 +57,87 @@ class VOCDataset_test(torch.utils.data.Dataset):
         return img_return, self.file_list[index]
 
 testset = VOCDataset_test(test_files, test_dir, mode='test')
-testloader = DataLoader(testset, batch_size = 1, shuffle=False, num_workers=0)
+testloader = DataLoader(testset, batch_size = BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
 
-filename_pth = 'save_model_sample.pth'
+filename_pth = 'save_weight.pth'
 model = torch.load(filename_pth)
 model.eval()
-# print(model)
-threshold = 0.2
+threshold = 0.5
 image_index = 0
-DEVICE = 'cpu'
-for img, index in testloader:
-    img = img.to(DEVICE)
-    output = model(img)
-    torch.set_printoptions(threshold=10000)   # input torch 확인용
-    # print(output.shape)
-    # output = output.squeeze()
-    output = torch.reshape(output, (7,7,30))
-    # print("output tensor size : ", output.shape)
 
-    img = img.squeeze() # batch size인 차원 삭제
-    img = np.transpose(img, (1,2,0)).numpy()    # opencv에 맞게 수정, 근데 0~1사이의 값인데도 잘 출력이니 냅두기
-    img = img * 255
-    img = img.astype(np.uint8)
-    img = np.ascontiguousarray(img, dtype=np.uint8)
+GPU_NUM = 2 # 원하는 GPU 번호 입력
+DEVICE = torch.device(f'cuda:{GPU_NUM}' if torch.cuda.is_available() else 'cpu')
+torch.cuda.set_device(DEVICE) # change allocation of current GPU
 
-    output = output.detach().numpy()
-    confidence1 = output[...,20]
-    # print(confidence1)
-    confidence2 = output[...,25]
-    where1 = np.transpose(np.nonzero(confidence1 > threshold))
-    # print(where1)
+for img_whole, index in testloader:
+    img_whole = img_whole.to(DEVICE)
+    # print(index)
+    for batchindex in range(img_whole.shape[0]):
+        img = img_whole[batchindex,...].unsqueeze(0)
+        output = model(img)
+        output = torch.reshape(output, (7,7,30))
+        # print("output tensor size : ", output.shape)
+        
+        img = img.cpu().detach().squeeze()
+        img = np.transpose(img, (1,2,0)).numpy()    # opencv에 맞게 수정, 근데 0~1사이의 값인데도 잘 출력이니 냅두기
+        img = img * 255
+        img = img.astype(np.uint8)
+        img = np.ascontiguousarray(img, dtype=np.uint8)
+        # print(img.shape)
+        output = output.cpu().detach().numpy()
+        confidence1 = output[...,20]
+        confidence2 = output[...,25]
+        # print("confidence1 : ", confidence1)
+        # print("confidence2 : ", confidence2)
+        where1 = np.transpose(np.nonzero(confidence1 > threshold))
+        # print(where1)
+        where2 = np.transpose(np.nonzero(confidence2 > threshold))
+        # print(output.shape)
+        for i in range(len(where1)):
+            x_grid = where1[i,0]
+            # print('x grid', x_grid)
+            y_grid = where1[i,1]
 
-    where2 = np.transpose(np.nonzero(confidence2 > threshold))
-    # print(where2)
+            class_index = np.argmax(output[x_grid,y_grid,:20])+1 # index를 맞추기 위하여 +1
+            # print(class_index)
+            grid_size = pixel_size / 7
+            # print(output[x_grid,y_grid,20:25])
+            x = output[x_grid,y_grid,21] * grid_size + x_grid*grid_size
+            y = output[x_grid,y_grid,22] * grid_size + y_grid*grid_size
+            w = output[x_grid,y_grid,23] * grid_size
+            h = output[x_grid,y_grid,24] * grid_size
+            # x = pixel_size/7 * x + output
+            # print(x, y, w, h)
+            draw = cv2.rectangle(img, (int(x-w/2), int(y-h/2)), (int(x+w/2), int(y+h/2)),
+            (0, 0, 255), 2)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            position = list_of_value.index(class_index)
+            # print("1번", list_of_key[position])
+            cv2.putText(img, list_of_key[position], (int(x-w/2), int(y-h/2)), font, 0.5, (255,255,255), 1)
+        
+        for i in range(len(where2)):
+            x_grid = where2[i,0]
+            y_grid = where2[i,1]
 
+            class_index = np.argmax(output[x_grid,y_grid,:20])+1 # index를 맞추기 위하여 +1
+            # print(class_index)
+            grid_size = pixel_size / 7
+            # print(output[x_grid,y_grid,25:30])
+            x = output[x_grid,y_grid,26] * grid_size + x_grid*grid_size
+            y = output[x_grid,y_grid,27] * grid_size + y_grid*grid_size
+            w = output[x_grid,y_grid,28] * pixel_size
+            h = output[x_grid,y_grid,29] * pixel_size
+            # x = pixel_size/7 * x + output
+            # print(x, y, w, h)
+            draw = cv2.rectangle(img, (int(x-w/2), int(y-h/2)), (int(x+w/2), int(y+h/2)),
+            (0, 0, 255), 2)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            position = list_of_value.index(class_index)
+            # print("2번", list_of_key[position])
+            cv2.putText(img, list_of_key[position], (int(x-w/2), int(y-h/2)), font, 0.5, (255,255,255), 1)
 
-    for i in range(len(where1)):
-        x_grid = where1[i,0]
-        y_grid = where1[i,1]
-
-        class_index = np.argmax(output[x_grid,y_grid,:20])+1 # index를 맞추기 위하여 +1
-        # print(class_index)
-        grid_size = pixel_size / 7
-        print(output[x_grid,y_grid,21:25])
-        x = output[x_grid,y_grid,21] * grid_size + x_grid*grid_size
-        y = output[x_grid,y_grid,22] * grid_size + y_grid*grid_size
-        w = output[x_grid,y_grid,23] * pixel_size
-        h = output[x_grid,y_grid,24] * pixel_size
-        # x = pixel_size/7 * x + output
-        # print(x, y, w, h)
-        draw = cv2.rectangle(img, (int(x-w/2), int(y-h/2)), (int(x+w/2), int(y+h/2)),
-        (0, 0, 255), 2)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        position = list_of_value.index(class_index)
-        cv2.putText(img, list_of_key[position], (int(x-w/2), int(y-h/2)), font, 0.5, (255,255,255), 1)
-    cv2.imshow('img', img)
-    # cv2.imwrite(f'image{image_index}.jpg', img)   # 서버에서 저장하는 용도로 사용!
-    image_index = image_index+1
-    cv2.waitKey(0)
-
-# confidence, indice = torch.max(output[...,:20], dim=2)
-# confidence = confidence.detach().numpy()
-# indice = indice.detach().numpy() + 1 # 0부터 시작하는 index를 맞춘 것
-
-
-  
-cv2.destroyAllWindows()
-
+        cv2.imwrite(f'image{image_index}.jpg', img)   # 서버에서 저장하는 용도로 사용!
+        # cv2.imwrite(f'{index[batchindex]}', img)
+        image_index = image_index+1
+print("finished!") 
 
